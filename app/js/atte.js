046972a7bc894881136050ada60c6d4db143b570
Max Prichinenko <max.prichinenko@gmail.com>
2013-11-07 20:54:48 +0200

    App | Enable translations

--- app/js/atte.js
+++ app/js/atte.js
@@ -1,2 +1,11 @@
-var app = angular.module('atte', ['ng']);
-alert('I\'m working!');
+var app = angular.module('atte', ['ng', 'ngCookies', 'pascalprecht.translate']);
+
+// Enable translations
+app.config(['$translateProvider', function($translate) {
+  $translate.useStaticFilesLoader({
+    prefix : 'l10n/',
+    suffix : '.json'
+  });
+  $translate.useLocalStorage();
+  $translate.preferredLanguage('en');
+}]);
